3aabf2f45f119966d6ece7bb7285d2acfe61b9bc
mateja <mateja176@gmail.com>
2019-11-17 13:19:13 +0100

    feat(tsconfig): issue with absolute path module resolution

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,6 @@
+// import App from 'App';
 import React from 'react';
+import { renderToString } from 'react-dom/server';
+import App from './App';
 
-console.log(<div>Hello</div>);
+console.log(renderToString(<App />));
