2ae11f7453c700d3711696f2490024fb6c3c560e
mateja <mateja176@gmail.com>
2019-11-17 13:27:35 +0100

    resets jsx to preserve in root tsconfig and sets jsx to react in src

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-// import App from 'App';
+import App from 'App';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-import App from './App';
+// import App from './App';
 
 console.log(renderToString(<App />));
