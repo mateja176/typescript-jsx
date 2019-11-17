f5901854b398a8965f69216de1025a14aa1f9ff0
mateja <mateja176@gmail.com>
2019-11-17 20:12:01 +0100

    adds preload.js

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
 
-const App: React.FC<{}> = () => <div>Hello {process.env.NAME}</div>;
+const App: React.FC = () => <div>Hello {process.env.NAME}</div>;
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import App from 'App';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-// import App from './App';
 
 console.log(renderToString(<App />));
